fd91996a25161bb41ad588dbb2ed7a7565e5f0ac
Shivasai <38298958+shivasai780@users.noreply.github.com>
2026-07-08 18:16:00 +0530

    Initial Project structure

---
+++ src/sql/01_setup/01_create_database.sql
@@ -0,0 +1,9 @@
+-- ===========================================================
+-- Project : Enterprise Banking Data Platform
+-- Module  : Environment Setup
+-- Script  : 01_create_database.sql
+-- Purpose : Create Development Database
+-- ===========================================================
+
+CREATE DATABASE IF NOT EXISTS BANKING_DEV
+COMMENT = 'Development database for Enterprise Banking Data Platform';
